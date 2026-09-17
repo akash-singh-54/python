923d09cb046434aad70c6b9c7f8410461072e255
akash-singh-54 <akashsingh.dataanalyst@gmail.com>
2026-09-17 07:20:26 -0700

    Refactor print statements in FirstClass.py

--- FirstClass.py
+++ FirstClass.py
@@ -1,7 +1,13 @@
-print("Hello World!")
-print("5+2")
-print(5+2)
-print(5<2)
-print(5>2)
-print(5==2)
-print("5+6= ", 5+6)
+print("Hello World!") # Result ==> Hello World!
+
+print("5+2")   # Result ==> 5+2
+
+print(5+2)   # Result ==> 7
+
+print(5<2)   # Result ==> False
+
+print(5>2)   # Result ==> True
+
+print(5==2)  # Result ==>  False
+
+print("5+6= ", 5+6)   # Result ==> 5+6= 11
