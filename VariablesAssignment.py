8398e398f37b17d2313b3685759df7da8731f57c
Akash Singh <akashsingh.dataanalyst@gmail.com>
2026-09-19 08:42:52 -0700

    Add files via upload

---
+++ VariablesAssignment.py
@@ -0,0 +1,59 @@
+# 1. Create a variable named 'course' and assign the Python value to it.
+
+course = "Python"
+print("The course name is ", course)
+print("\n\n")
+
+
+# 2. Create a variable where the values are in string, float, and integer.
+
+str1 = "Hello World"
+float1 = 31.34
+int1 = 62
+print("The string is", str1, "\nThe decimal value is", float1, "\nThe integer value is", int1)
+print("\n\n")
+
+
+# 3. Write a program where the output should be : (11,12,13) (11,12,13) (11,12,13)
+
+x=y=z= 11,12,13
+print(x, y, z)
+print("\n\n")
+
+
+# 4. Find the error and correct it:- 
+#       a_1b__c&b=“akash”
+#       __a_b c d=1234
+#       1@2&c=“python”
+
+a_1b__cb = "akash"
+__a_bcd = 1234
+c12 = "python"
+
+print(a_1b__cb, "\n",__a_bcd, "\n", c12)
+print("\n\n")
+
+
+
+# 5. Create multiple values in one line of three states and give the capitals of these states as values. 
+
+Uttarakhand,Rajesthan,Mharashtra = "Dehradun","Jaipur","Mumbai"
+print("Capital of Uttarakhand is", Uttarakhand, "\nCapital of Rajesthan is", Rajesthan, "\nCapital of Maharashtra is", Mharashtra)
+print("\n\n")
+
+
+
+# 6. Create a variable to check case sensitivity and correct it.
+
+#      Xyz="welcome to India"
+#      print(xyz)
+
+#     _objeCt="python"
+#     print(_Object)
+
+
+Xyz="welcome to India"
+print(Xyz)
+
+_objeCt="python"
+print(_objeCt)
