c38803b3070fc4b1f313fef29fe3fa2f64761231
Akash Singh <akashsingh.dataanalyst@gmail.com>
2026-09-23 07:32:10 -0700

    Update 6_DataType.py

--- 6_DataType.py
+++ 6_DataType.py
@@ -14,53 +14,53 @@
 #immutable :- 
 
 # int :-  number vlaues 
-# num1 = 100 
-# # #use 'type' to check the types of data 
-# print(num1, type(num1))
+num1 = 100 
+# #use 'type' to check the types of data 
+print(num1, type(num1))
 
 
 # #float :- decimal value 
-# num2 = 10.9
-# print(num2 , type(num2))
+num2 = 10.9
+print(num2 , type(num2))
 
 
 # # boolean :- output in true or false 
 
-# num3 = True 
-# print(num3, type(num3))
-# num4 = 100 > 50
-# print(num4, type(num4))
+num3 = True 
+print(num3, type(num3))
+num4 = 100 > 50
+print(num4, type(num4))
 
 
-# #string ;- collection of character in single , dobule , triple quote 
-# str1 = 'codedesk'
-# st2 = "codedesk"
-# str3 = '''
-#  hello
-#   codedesk
-# '''
-# print(str3)
-# #use type 
-# print(type(st2))
+ #string ;- collection of character in single , dobule , triple quote 
+str1 = 'codedesk'
+st2 = "codedesk"
+str3 = '''
+  hello
+   codedesk
+'''
+print(str3)
+ #use type 
+print(type(st2))
 
 
 # #string indexing :- use to get the single charcters 
 
-# str4 = "i am a python developer"
+str4 = "i am a python developer"
 
 # # # for total length :- start counting from 1 
 # # # for total indexing - start counting from 0 
 
-# print(str4[0])
-# print(str4[7])
+print(str4[0])
+print(str4[7])
 
 # # #reverse indexing :- 
-# print(str4[-1])
+print(str4[-1])
 
 
 # #total length :- use len function 
-# str4 = "i am a python developer dkljvk lxcnvkld snvkld snvkldsn vlkds nvkldfsn vkldsfn vkldsn kvldsn kvlsdlvk dslvn"
-# print(len(str4))
+str4 = "i am a python developer dkljvk lxcnvkld snvkld snvkldsn vlkds nvkldfsn vkldsfn vkldsn kvldsn kvlsdlvk dslvn"
+print(len(str4))
 
 # string slicing :- use to get the multiple characters
 str5 = "i am a python developer"
